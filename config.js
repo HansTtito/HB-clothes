89edc7ca6c98fa506e85c3a3e3230b7a98e7ad07
HansTtito <kvttitos@gmail.com>
2026-05-01 19:07:10 -0400

    feat(home): categorias destacadas con filtros dinamicos por genero y nuevos

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const HB_CONFIG = {
   apiBaseUrl: "https://pcqeipgl3m.execute-api.us-east-1.amazonaws.com",
   cdnBaseUrl: "https://hb-clothes-assets.s3.us-east-1.amazonaws.com",
-  shippingCost: 100,
+  shippingCost: 3999,
   currency: "CLP",
   brand: "HB clothes",
   whatsapp: "56969004163"
